d95fd2c404e4bd3f6dc3d184e0dcf07981d75c78
EvelynNiksa <evelyniksa@gmail.com>
2023-02-25 10:15:57 -0500

    file save code complete

--- app/src/main/java/com/example/pitscouting2023_legacysupport/PictureScreen.java
+++ app/src/main/java/com/example/pitscouting2023_legacysupport/PictureScreen.java
@@ -3,6 +3,7 @@ package com.example.pitscouting2023_legacysupport;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +11,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class PictureScreen extends AppCompatActivity {
-    Button frontCaptureBtn, backCaptureBtn, sideCaptureBtn;
+    Button frontCaptureBtn, backCaptureBtn, sideCaptureBtn, frontSaveBtn, backSaveBtn, sideSaveBtn;
     ImageView frontImageView, backImageView, sideImageView;
     private static final int front_pic_id = 100;
     private static final int back_pic_id = 101;
     private static final int side_pic_id = 102;
+    Bitmap frontImageBitmap, backImageBitmap, sideImageBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,9 @@ public class PictureScreen extends AppCompatActivity {
         frontCaptureBtn = findViewById(R.id.captureFrontBtn);
         backCaptureBtn = findViewById(R.id.captureBackBtn);
         sideCaptureBtn = findViewById(R.id.captureSideBtn);
+        frontSaveBtn = findViewById(R.id.saveFrontPicBtn);
+        backSaveBtn = findViewById(R.id.saveBackPicBtn);
+        sideSaveBtn = findViewById(R.id.saveSidePicBtn);
 
         frontImageView = findViewById(R.id.frontPic);
         backImageView = findViewById(R.id.backPic);
@@ -37,8 +48,8 @@ public class PictureScreen extends AppCompatActivity {
         frontCaptureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(camera_intent, front_pic_id);
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, front_pic_id);
             }
         });
 
@@ -57,27 +68,74 @@ public class PictureScreen extends AppCompatActivity {
                 startActivityForResult(camera_intent, side_pic_id);
             }
         });
-    }
 
+        frontSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //need to change file name so it saves the team # and what robot view it is
+                String frontImageFileName = "teamnumber" + "FrontView";
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                File frontImageFile = new File(storageDir, frontImageFileName + ".jpg");
+
+                try (OutputStream outputStream = new FileOutputStream(frontImageFile)) {
+                    frontImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        backSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //need to change file name so it saves the team # and what robot view it is
+                String backImageFileName = "teamnumber" + "BackView";
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                File backImageFile = new File(storageDir, backImageFileName + ".jpg");
+
+                try (OutputStream outputStream = new FileOutputStream(backImageFile)) {
+                    backImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        sideSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //need to change file name so it saves the team # and what robot view it is
+                String sideImageFileName = "teamnumber" + "SideView";
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                File sideImageFile = new File(storageDir, sideImageFileName + ".jpg");
+
+                try (OutputStream outputStream = new FileOutputStream(sideImageFile)) {
+                    sideImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // Match the request 'pic id with requestCode
         if (requestCode == front_pic_id) {
             // BitMap is data structure of image file which store the image in memory
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            frontImageBitmap = (Bitmap) data.getExtras().get("data");
             // Set the image in imageview for display
-            frontImageView.setImageBitmap(photo);
-        }
-        else if(requestCode == back_pic_id) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            frontImageView.setImageBitmap(frontImageBitmap);
+        } else if (requestCode == back_pic_id) {
+            backImageBitmap = (Bitmap) data.getExtras().get("data");
             // Set the image in imageview for display
-            backImageView.setImageBitmap(photo);
-        }
-        else if(requestCode == side_pic_id) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            backImageView.setImageBitmap(backImageBitmap);
+        } else if (requestCode == side_pic_id) {
+            sideImageBitmap = (Bitmap) data.getExtras().get("data");
             // Set the image in imageview for display
-            sideImageView.setImageBitmap(photo);
+            sideImageView.setImageBitmap(sideImageBitmap);
         }
 
     }
